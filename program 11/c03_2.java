535e6db72958ff1902968edf61e5576c41f1396e
adithyanmkohm-web <adithyanmkohm@gmail.com>
2026-03-25 11:47:36 +0530

    Added my java programs

---
+++ program 11/c03_2.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+class Employee {
+    int id;
+    String name;
+    int sal;
+    String address;
+
+    Employee(int id, String name, int sal, String address) {
+        this.id = id;
+        this.name = name;
+        this.sal = sal;
+        this.address = address;
+    }
+
+    public void display() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + sal);
+        System.out.println("Employee Address: " + address);
+    }
+}
+
+class Teacher extends Employee {
+    String dept;
+    String sub;
+
+    Teacher(int id, String name, int sal, String address, String dept, String sub) {
+        super(id, name, sal, address);
+        this.dept = dept;
+        this.sub = sub;
+    }
+
+    public void display() {
+        super.display();
+        System.out.println("Department: " + dept);
+        System.out.println("Subject: " + sub);
+        System.out.println();
+    }
+}
+
+public class c03_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of teachers: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Teacher[] t = new Teacher[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for Teacher " + (i + 1));
+
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Salary: ");
+            int sal = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Address: ");
+            String address = sc.nextLine();
+
+            System.out.print("Department: ");
+            String dept = sc.nextLine();
+
+            System.out.print("Subject: ");
+            String sub = sc.nextLine();
+
+            t[i] = new Teacher(id, name, sal, address, dept, sub);
+        }
+
+        System.out.println("\nTeacher Details:\n");
+
+        for (int i = 0; i < n; i++) {
+            t[i].display();
+        }
+
+        sc.close();
+    }
+}
---
+++ program 17/TestGra.java
@@ -0,0 +1,10 @@
+import Graphics.Area;
+public class TestGra{
+public static void main(String[] args){
+Area g =new Area();
+System.out.println("area of rectangle:"+g.rect(4,5));
+System.out.println("area of triangle:"+g.tria(3,4.5));
+System.out.println("area of Square:"+g.squ(5));
+System.out.println("area of circle:"+g.cir(4.5));
+}
+}
